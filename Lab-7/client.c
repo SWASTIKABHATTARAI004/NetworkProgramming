74f0e2381f53d8f1b20b777b20f295fa71c5aea8
SWASTIKABHATTARAI004 <bhattaraiswastika19@gmail.com>
2026-07-10 19:15:23 +0545

    Add Lab 7

---
+++ Lab-7/client.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main()
+{
+    int sock;
+    struct sockaddr_in server;
+    char buffer[1024];
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+
+    inet_pton(AF_INET, "127.0.0.1", &server.sin_addr);
+
+    connect(sock,
+            (struct sockaddr *)&server,
+            sizeof(server));
+
+    char message[] = "Hello Server";
+
+    write(sock,
+          message,
+          strlen(message));
+
+    read(sock,
+         buffer,
+         sizeof(buffer));
+
+    printf("Server: %s\n", buffer);
+
+    close(sock);
+
+    return 0;
+}
